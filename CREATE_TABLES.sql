241204f40f5309e06ee14ce69d3f026b1f0afc16
Rafael A O Paulucci <rafaelpaulucci2016@gmail.com>
2020-04-24 11:25:00 -0300

    Fix types for Observation

--- CREATE_TABLES.sql
+++ CREATE_TABLES.sql
@@ -2,14 +2,14 @@ BEGIN TRAN
 CREATE TABLE [MicroWeather] (
     [timestamp] DateTime2 primary key NOT NULL,
     [altitude] int not null,
-    [temperature] int not null,
+    [temperature] float not null,
     [humidity] int not null,
     [pressure] int not null,
-    [wind_speed] int not null,
+    [wind_speed] float not null,
     [wind_direction] varchar(3) not null,
     [soil_moisture] int not null,
-    [soil_temp] int not null,
-    [rain_rate] int not null,
+    [soil_temp] float not null,
+    [rain_rate] float not null,
     [image] varchar(max)
 );
 COMMIT
--- microweather/Models/Observation.cs
+++ microweather/Models/Observation.cs
@@ -13,21 +13,21 @@ namespace microweather.Models
 
         public int altitude { get; set; }
 
-        public int temperature { get; set; }
+        public float temperature { get; set; }
 
         public int humidity { get; set; }
 
         public int pressure { get; set; }
 
-        public int wind_speed { get; set; }
+        public float wind_speed { get; set; }
 
         public string wind_direction { get; set; }
 
         public int soil_moisture { get; set; }
 
-        public int soil_temp { get; set; }
+        public float soil_temp { get; set; }
 
-        public int rain_rate { get; set; }
+        public float rain_rate { get; set; }
 
         public string image { get; set; }
 
